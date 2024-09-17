afa748cf41ac429997919bddce6e044412b4af5c
Miki Tebeka <miki@353solutions.com>
2024-09-17 09:50:43 +0300

    proto files

---
+++ unter_pb2.pyi
@@ -0,0 +1,28 @@
+from google.protobuf import timestamp_pb2 as _timestamp_pb2
+from google.protobuf.internal import enum_type_wrapper as _enum_type_wrapper
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Mapping as _Mapping, Optional as _Optional, Union as _Union
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class Kind(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
+    __slots__ = ()
+    KIND_UNSPECIFIED: _ClassVar[Kind]
+    KIND_REGULAR: _ClassVar[Kind]
+    KIND_SHARED: _ClassVar[Kind]
+KIND_UNSPECIFIED: Kind
+KIND_REGULAR: Kind
+KIND_SHARED: Kind
+
+class StartRideRequest(_message.Message):
+    __slots__ = ("id", "kind", "driver_id", "time")
+    ID_FIELD_NUMBER: _ClassVar[int]
+    KIND_FIELD_NUMBER: _ClassVar[int]
+    DRIVER_ID_FIELD_NUMBER: _ClassVar[int]
+    TIME_FIELD_NUMBER: _ClassVar[int]
+    id: str
+    kind: Kind
+    driver_id: str
+    time: _timestamp_pb2.Timestamp
+    def __init__(self, id: _Optional[str] = ..., kind: _Optional[_Union[Kind, str]] = ..., driver_id: _Optional[str] = ..., time: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ...) -> None: ...
